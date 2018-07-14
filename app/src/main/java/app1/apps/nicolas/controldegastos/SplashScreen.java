1e520cf3bf1eec56f9906a07b5612482346c09f6
nicolas cantor <nicolascantor103@gmail.com>
2018-07-14 08:18:20 -0500

    avance 14072018 hora 817am
    primer avance, se realiza shareppreference y se crean los actities

--- app/src/main/java/app1/apps/nicolas/controldegastos/SplashScreen.java
+++ app/src/main/java/app1/apps/nicolas/controldegastos/SplashScreen.java
@@ -1,6 +1,8 @@
 package app1.apps.nicolas.controldegastos;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,12 +14,40 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(intent);
+                
+                SharedPreferences primerIngreso = getSharedPreferences("primerIngreso", Context.MODE_PRIVATE);
+                String verificacion = primerIngreso.getString("primerIngreso", "primera vez");
+
+                if(verificacion=="primera vez"){
+
+                    SharedPreferences.Editor editor = primerIngreso.edit();
+                    editor.putString("primera vez", "versio 1.0.0 accedido xjhfhdsls354fg4srg");
+                    editor.commit();
+
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+
+                }else {
+
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+
+                }
+
+
+
             }
         }, 6000);
+
+
+
     }
+
+
+
 }
