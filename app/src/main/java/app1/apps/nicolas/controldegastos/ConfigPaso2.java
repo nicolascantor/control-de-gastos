ed0b8ed41fef88ffca48363f7956b24d24f3aa17
nicolascantor <nicolascantor103@gmail.com>
2018-07-17 23:07:48 -0500

    cuadroDialogo
    se realiza el cuadro de dialogo para el ingreso del valor

--- app/src/main/java/app1/apps/nicolas/controldegastos/ConfigPaso2.java
+++ app/src/main/java/app1/apps/nicolas/controldegastos/ConfigPaso2.java
@@ -1,59 +1,47 @@
 package app1.apps.nicolas.controldegastos;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class ConfigPaso2 extends AppCompatActivity {
+public class ConfigPaso2 extends AppCompatActivity implements CuadroDialogoNuevoValor.finalizoCuadroDialgo{
+
+    Context contexto;
+    TextView nombreIngresado, valorIngresado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_paso2);
-        final EditText nombre = new EditText(this);
-        final EditText valor = new EditText(this);
 
-        nombre.setHint("Nombre");
-        valor.setHint("Valor.");
+        nombreIngresado = (TextView) findViewById(R.id.textNombre);
+        valorIngresado  = (TextView) findViewById(R.id.textValor);
+
+        contexto = this;
 
 
-        Button agregarIngreso = findViewById(R.id.button);
+        Button agregarIngreso = findViewById(R.id.btnAgregarMas);
+
         agregarIngreso.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ConfigPaso2.this);
 
-                // Configura el titulo.
-                alertDialogBuilder.setTitle("Agregar Ingreso");
-
-                // Configura el mensaje.
-                alertDialogBuilder
-                        .setMessage("Por favor ingrese ")
-                        .setView(nombre)
-                        .setView(valor)
-                        .setCancelable(false)
-                        .setPositiveButton("Si",new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int id) {
-
-                                //Si la respuesta es afirmativa aquí agrega tu función a realizar.
-                            }
-                        })
-                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int id) {
-                                dialog.cancel();
-                            }
-                        }).create().show();
+                new CuadroDialogoNuevoValor(contexto, ConfigPaso2.this);
             }
         });
     }
 
 
-
+    @Override
+    public void resultadoCuadroDialogo(String nombre, String valor) {
+        nombreIngresado.setText(nombre);
+        valorIngresado.setText(valor);
+    }
 }
 
---
+++ app/src/main/java/app1/apps/nicolas/controldegastos/CuadroDialogoNuevoValor.java
@@ -0,0 +1,49 @@
+package app1.apps.nicolas.controldegastos;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CuadroDialogoNuevoValor {
+
+    public interface finalizoCuadroDialgo{
+        void resultadoCuadroDialogo(String nombre, String valor);
+    }
+
+    private finalizoCuadroDialgo interfaz;
+
+    public CuadroDialogoNuevoValor(Context contexto, finalizoCuadroDialgo actividad){
+
+        interfaz = actividad;
+
+        final Dialog dialogoNuevoValor = new Dialog(contexto);
+        dialogoNuevoValor.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialogoNuevoValor.setCancelable(false);
+        dialogoNuevoValor.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialogoNuevoValor.setContentView(R.layout.dialog_config_paso2);
+
+        final EditText nombre = (EditText) dialogoNuevoValor.findViewById(R.id.nombreNuevoValor);
+        final EditText valor = (EditText) dialogoNuevoValor.findViewById(R.id.nuevoValor);
+        Button btnIngresarNuevoValor = (Button) dialogoNuevoValor.findViewById(R.id.btnNuevoValor);
+
+        valor.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+        btnIngresarNuevoValor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                interfaz.resultadoCuadroDialogo(nombre.getText().toString(), valor.getText().toString());
+                dialogoNuevoValor.dismiss();
+            }
+        });
+
+        dialogoNuevoValor.show();
+
+    }
+}
