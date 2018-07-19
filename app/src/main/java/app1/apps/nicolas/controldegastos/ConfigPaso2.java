203b7ea7c20014c226ca415cf387b9d78a5ea531
nicolas cantor <nicolascantor103@gmail.com>
2018-07-19 14:44:15 -0500

    ACTUALIZACION
    ACTUALIZACION

--- app/src/main/java/app1/apps/nicolas/controldegastos/ConfigPaso2.java
+++ app/src/main/java/app1/apps/nicolas/controldegastos/ConfigPaso2.java
@@ -5,22 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ConfigPaso2 extends AppCompatActivity implements CuadroDialogoNuevoValor.finalizoCuadroDialgo{
 
     Context contexto;
     TextView nombreIngresado, valorIngresado;
+    ListView list;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_paso2);
 
-        nombreIngresado = (TextView) findViewById(R.id.textNombre);
-        valorIngresado  = (TextView) findViewById(R.id.textValor);
+        list = (ListView) findViewById(R.id.listaIngresos);
+
+        arrayList = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.spinner_item_layout, arrayList);
+        list.setAdapter(adapter);
+
 
         contexto = this;
 
@@ -40,8 +52,12 @@ public class ConfigPaso2 extends AppCompatActivity implements CuadroDialogoNuevo
 
     @Override
     public void resultadoCuadroDialogo(String nombre, String valor) {
-        nombreIngresado.setText(nombre);
-        valorIngresado.setText(valor);
+
+        //nombreIngresado.setText(nombre);
+        //valorIngresado.setText(valor);
+        arrayList.add(nombre);
+        adapter.notifyDataSetChanged();
+
     }
 }
 
